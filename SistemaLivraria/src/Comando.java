b13cd67765755b3d4e8398692e97df75ecece43a
Breno Cupertino <brenonscupertino@gmail.com>
2025-02-01 10:49:54 -0300

    feat: change src directory

--- SistemaLivraria/src/Comando.java
+++ SistemaLivraria/src/Comando.java
@@ -1,5 +1,4 @@
 
 public interface Comando {
 	public void executar(CarregadorParametros carregadorParametros);
-
 }
--- SistemaLivraria/src/InterfaceUsuario.java
+++ SistemaLivraria/src/InterfaceUsuario.java
@@ -2,7 +2,11 @@ import java.util.HashMap;
 
 public class InterfaceUsuario {
 	private HashMap<String,Comando> comandos = new HashMap<String,Comando>();
-	
+
+	public InterfaceUsuario() {
+		inicializarComandos();
+	}
+
 	private void inicializarComandos() {
 		comandos.put("emp", new EmprestarComando());
 		comandos.put("usu", new ConsultarUsuarioComando());
